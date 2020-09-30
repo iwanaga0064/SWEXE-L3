4ddcd803b60d9acf8568168213ee05acf22463ab
Tomoki Iwanaga <1811140064f@ed.fuk.kindai.ac.jp>
2020-09-30 05:00:04 +0000

    initial commit

---
+++ app/controllers/tweets_controller.rb
@@ -0,0 +1,41 @@
+class TweetsController < ApplicationController
+  def index
+    @tweets = Tweet.all
+  end
+  
+  def new
+    @tweet = Tweet.new
+  end
+  
+  def create
+    message = params[:tweet][:message]
+    tdate = Time.current
+    @tweet = Tweet.new(message: message, tdate: tdate)
+    if @tweet.save
+      flash[:notice] = 'ツイートできました'
+      redirect_to '/'
+    else
+      render 'new'
+    end
+  end
+  
+  def show
+    @tweet = Tweet.find(params[:id])
+  end
+  
+  def edit
+    @tweet = Tweet.find(params[:id])
+  end
+  
+  def update
+    tweet = Tweet.find(params[:id])
+    tweet.update(message: params[:tweet][:message])
+    redirect_to '/'
+  end
+  
+  def destroy
+    tweet = Tweet.find(params[:id])
+    tweet.destroy
+    redirect_to '/'
+  end
+end
